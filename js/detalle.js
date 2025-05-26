99046980e633739b45a33b79f34113621aaa8957
Lucaledesma <lucaledesma55@gmail.com>
2025-05-26 16:02:28 -0300

    Ver detalles con dos Archivos .json

--- js/detalle.js
+++ js/detalle.js
@@ -5,16 +5,35 @@ window.addEventListener('load', function(){
     let uid = (location.search).replace('?','');
     console.log(uid);
     
-    const pedirAlojamientos = async () => {
+    const pedirDestacados = async () => {
     const resp = await fetch ("../js/json/destacados.json");
     let alojamientos = await resp.json();
     return alojamientos;
     }
 
-    let alojamientos = pedirAlojamientos();
+    const pedirBariloche = async () => {
+    const resp = await fetch ("../js/json/bariloche.json");
+    let alojamientos = await resp.json();
+    return alojamientos;
+    }
+
+    let destacados = pedirDestacados();
+    let bariloche = pedirBariloche();
+    let alojamientos = Promise.all([destacados, bariloche]);
+
     alojamientos
     .then ( alojamientos => {
-        const alojamientoAux = alojamientos.find(alojamiento => alojamiento.id === uid);
+
+        const [dest, bari] = alojamientos;
+
+        console.log(dest);
+        console.log(bari);
+
+        let alojamientoAux = dest.find(des => des.id === uid);
+
+        if (!alojamientoAux){
+            alojamientoAux = bari.find(bar => bar.id === uid);
+        }
 
         detalleContenedor.innerHTML = `
         <h1>${alojamientoAux.titulo}</h1>
