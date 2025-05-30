f008e731751b4615ae8150fbcf625e93f4b85ca4
Ezequiel Ignacio de la Fuente <ezequieldelafuente2001@gmail.com>
2025-05-29 23:45:01 -0300

    1) Contactar al publicar un alojamiento

---
+++ js/components/header-component.js
@@ -0,0 +1,40 @@
+class SiteHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+          <header>
+      <div class="logo">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_Bélo.svg"
+            alt="Airbnb Logo"
+          />
+        </a>
+      </div>
+
+      <!-- Buscador -->
+      <div class="search-box">
+        <span>¿Dónde?</span>
+        <span>¿Cuándo?</span>
+        <span>¿Cuántos?</span>
+        <span class="search-icon">🔍</span>
+      </div>
+
+      <!-- Iconos derechos -->
+      <div class="right-icons">
+        <a
+          class="button-hover"
+          href="/pages/contactoConvierteteAnfitrion.html"
+        >
+          <button>Hazte anfitrión</button>
+        </a>
+        <a href="/pages/login.html" class="button">
+          <button>Iniciar Sesion</button>
+        </a>
+        <a href="/pages/registro.html" class="button">
+          <button>Registrarse</button>
+        </a>
+      </div>
+    </header>`;
+  }
+}
+customElements.define("site-header", SiteHeader);
