3e8dc77ed78f64d42f15dfb48d1cb4b1edd8debc
Lucaledesma <lucaledesma55@gmail.com>
2025-05-30 11:56:14 -0300

    Funcionalidad de la confirmacion de la reserva

---
+++ js/confirmacion.js
@@ -0,0 +1,54 @@
+import { uid, alojamientos} from "./detalle.js";
+
+let confirmacionContenedor = document.querySelector("#confirmacionContenedor");
+
+alojamientos
+    .then(alojamientos => {
+        const alojamientoAux = alojamientos.find(alojamiento => alojamiento.id === uid);
+        console.log(uid);
+
+        const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
+        const reserva = reservas.find(reserva => reserva.uid === uid);
+
+        const { imagen, titulo, precio } = alojamientoAux;
+        const { start, end, days, adultos, ninos, bebes, mascotas } = reserva;
+
+        if (confirmacionContenedor)
+            confirmacionContenedor.innerHTML += `
+        <div>
+            <div class="summary-card">
+                <div class="listing-header">
+                    <img src="${imagen}"
+                        alt="Foto del alojamiento">
+                    <div class="listing-info">
+                        <h3>${titulo}</h3>
+                    </div>
+                </div>
+
+                <p style="font-size: 13px;">Esta reserva no es reembolsable. <a href="#">Política completa</a></p>
+
+                <div class="section">
+                    <div class="section-title">
+                        Información del viaje
+                    </div>
+                    <p>Desde: ${start} - Hasta: ${end}</p>
+                    <p>Adultos: ${adultos} - Niños: ${ninos} <br> Bebes: ${bebes} - Mascotas: ${mascotas}</p>
+                </div>
+
+                <hr style="margin: 16px 0; border: none; border-top: 1px solid #eee;">
+
+                <div class="section">
+                    <div class="section-title">Detalles del precio</div>
+                    <div class="price-details">
+                        <span>$${precio} USD por <span>${days}</span> noche/s</span>
+                        <span>$${precio * days} USD</span>
+                    </div>
+                    <div class="price-details total">
+                        <span>Total <a href="#">USD</a></span>
+                        <span>$${precio * days} USD</span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            `;
+    });
--- js/detalle.js
+++ js/detalle.js
@@ -1,59 +1,63 @@
 let detalleContenedor = document.querySelector("#detalleAlojamiento");
 
-window.addEventListener('load', function(){
+export let uid = (location.search).replace('?', '');
 
-    let uid = (location.search).replace('?','');
-    
-    const pedirAlojamientos = async () => {
-    const resp = await fetch ("../js/json/alojamientos.json");
+export const pedirAlojamientos = async() => {
+    const resp = await fetch("../js/json/alojamientos.json");
     let alojamientos = await resp.json();
     return alojamientos;
-    }
+}
 
-    let alojamientos = pedirAlojamientos();
-    alojamientos
-    .then ( alojamientos => {
+export let alojamientos = pedirAlojamientos();
+
+alojamientos
+    .then(alojamientos => {
         const alojamientoAux = alojamientos.find(alojamiento => alojamiento.id === uid);
-        console.log(uid);
 
-        const {imagen, titulo, precio, huespedes, descripcion, diasDisponibles, puntuacion} = alojamientoAux;
+        const {id, imagen, titulo, precio, huespedes, descripcion, diasDisponibles } = alojamientoAux;
 
         document.title = titulo;
 
         let precioReserva = document.querySelector("#precio-reserva");
 
-        precioReserva.textContent = `$${precio} USD`;
+        if (precioReserva)
+            precioReserva.textContent = `$${precio} USD`;
 
-        detalleContenedor.innerHTML = `
-        <div class="container">  
-            <div class="header">${titulo}</div>
-                <div class="images">
-                    <div class="main-image">
-                        <img src="${imagen}" alt="Interior principal">
+        if (detalleContenedor)
+            detalleContenedor.innerHTML = `
+            <div class="container">  
+                <div class="header">${titulo}</div>
+                    <div class="images">
+                        <div class="main-image">
+                            <img src="${imagen}" alt="Interior principal">
+                        </div>
                     </div>
+
+                    <div class="bottom">
+                        <div class="title_description">
+                            <div class="details">
+                                <h2>${titulo}</h2>
+                                <div class="info">${huespedes} huéspedes  ·  ${diasDisponibles} noches</div>
+                            </div>
+
+                            <div class="ratings">
+                                <div>🏅 Favorito entre huéspedes</div>
+                            </div>
+
+                            <div class="description">${descripcion}</div>
+                            <button class="show-more">Mostrar más</button>
+                        </div>
+                    </div>
+                    
                 </div>
 
-                <div class="bottom">
-                    <div class="title_description">
-                        <div class="details">
-                            <h2>${titulo}</h2>
-                            <div class="info">${huespedes} huéspedes  ·  ${diasDisponibles} noches</div>
-                        </div>
-
-                        <div class="ratings">
-                            <div>🏅 Favorito entre huéspedes</div>
-                        </div>
-
-                        <div class="description">${descripcion}</div>
-                        <button class="show-more">Mostrar más</button>
-                    </div>
-                </div>
-                
             </div>
+            `;
 
-        </div>
-        `;
+        let referenciaConfirmacion = document.querySelector("#referencia-confirmacion");
 
-    });
+        if (referenciaConfirmacion)
+            referenciaConfirmacion.setAttribute("href", `./confirmacionReserva.html?${id}`);
 
 });
+
